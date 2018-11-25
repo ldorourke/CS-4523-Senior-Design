6809926564b3d1100c37bb5034acc724b6c127e2
Farhad Ahmed <fa961@nyu.edu>
2018-11-25 16:15:08 -0500

    some basic html files

--- seniordesign/urls.py
+++ seniordesign/urls.py
@@ -3,10 +3,13 @@ from django.urls import path
 
 from . import views
 
+
 app_name = 'seniordesign'
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^seniordesign/index/*$', views.index, name='index'),
+    url(r'^seniordesign/dashboard/*$', views.dashboard, name='dashboard'),
+    url(r'^seniordesign/interests/*$', views.interests, name='interests'),
 ]
  
--- seniordesign/views.py
+++ seniordesign/views.py
@@ -12,3 +12,10 @@ site_hdr = "Senior Design"
 def index(request):
     return render(request, 'index.html', {'header': site_hdr})
 
+
+def dashboard(request):
+    return render(request, 'dashboard.html', {'header': site_hdr})
+
+def interests(request):
+    return render(request, 'interests.html', {'header': site_hdr})
+
