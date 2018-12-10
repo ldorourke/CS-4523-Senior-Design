b7ecd488eaeb5fcd83d0d32c567a7c672297c0c5
Farhad Ahmed <fa961@nyu.edu>
2018-12-10 17:13:03 -0500

    working on homepage html, added sidebar, make middle a feed type thing

--- seniordesign/urls.py
+++ seniordesign/urls.py
@@ -19,10 +19,11 @@ urlpatterns = [
     url(r'^register/$', userCreate ,name ="register"),
     url(r'^createEvent/$', userCreate ,name ="createEvent"),
     url(r'^viewEvent/$', userCreate ,name ="viewEvent"),
-    
     url(r'^interests/*$', views.interests, name='interests'),
+               
+               
     #The next two need their template names, add the names after the html files are added to proj
-    url(r'^login/$', LoginView.as_view(template_name = "home_logged_out copy.html",redirect_field_name = "/logged_in"), name='login'),
+    
     url(r'^loginErrors/$', LoginView.as_view(template_name = ".html",redirect_field_name = "/logged_in"), name='loginErrors'),
     
     url(r'^log_out/$', logout_view ,name ="log_out"),
